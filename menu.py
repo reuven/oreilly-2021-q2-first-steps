eaccb4e96c6f597ebe91c46dd4697f30e55e1fe5
Reuven Lerner <reuven@lerner.co.il>
2021-06-10 23:22:57 +0300

    gitautosync update 1

--- menu.py
+++ menu.py
@@ -6,3 +6,16 @@ def menu(*args):
             return choice
 
         print(f'Illegal choice; try again')
+
+
+# when I say "import menu" from another file, the __name__ for this module is 'menu'.
+# When it is run from the command line, the __name__ for this module is '__main__'.
+
+# I can use that distinction to have functionality that only runs when
+# the module/program is run interactively.
+
+if __name__ == '__main__':
+    print('Demo menu usage!')
+
+    user_choice = menu('a', 'b', 'c')
+    print(f'User choice was {user_choice}')
