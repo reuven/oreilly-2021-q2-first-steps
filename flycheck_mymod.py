cf439fbcb03f30a8d6e6c92228c5d91b366d3f9e
Reuven Lerner <reuven@lerner.co.il>
2021-06-10 21:54:02 +0300

    gitautosync update 80

---
+++ flycheck_mymod.py
@@ -0,0 +1,13 @@
+print(f'Hello from {__name__}!')
+
+x = 100
+
+y = [10, 20, 30]
+
+
+def hello(name):
+    return f'Hello, {name}!'
+
+
+if __name__ == '__main__':
+print(f'Goodbye from {__name__}!')
